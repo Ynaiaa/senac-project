14b61b1a7ca3cdef81125614381f399205e49ff3
Ynaiaa <kaylaine00@hotmail.com>
2024-06-02 20:03:47 -0300

    alteração 1

--- senac-project/app.js
+++ senac-project/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');  // Importando o cors
 const router = require('./routes/router');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());  // Usando o cors
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
